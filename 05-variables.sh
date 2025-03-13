d3d1e9912ba6cb36be4262e7c60cb5cc037d9d8a
Ajay2625 <ajayreddykondeti@gmail.com>
2025-03-12 19:49:07 -0700

    conversation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "please enter your Username::"
-read -s USERNAME # Here USERNAME  is the variable
+read USERNAME # Here USERNAME  is the variable
 
 echo "username entered: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
