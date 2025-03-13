2f37aa56863f7ca21617606161223385ef52403c
Ajay2625 <ajayreddykondeti@gmail.com>
2025-03-12 20:42:27 -0700

    conversation added

---
+++ 08-special-vars.sh
@@ -0,0 +1,8 @@
+All variables passed: $@ 
+Number of variables: $#
+script name: $0
+present working directory: $PWD
+home directory of current user: $HOME
+which user is running this script: $USER
+process id of current script: $$
+process id of last command in background: $!
