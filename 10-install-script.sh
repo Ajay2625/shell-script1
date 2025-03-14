49ccbed40c82f0b8946ef91c2066317a7fe4c8ee
Ajay2625 <ajayreddykondeti@gmail.com>
2025-03-13 20:49:55 -0700

    conversation added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,25 +8,34 @@ then
     echo "You must have sudo access to execute this script"
     exit 1 # other than 0
 fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+echo "MySQL is already ... installed"
 
-dnf install mysql -y
+fi
+
+dnf list installed git
 
 if [ $? -ne 0 ]
 then
-    echo "Installing MySQL... FAILURE"
-    exit 1
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
 else
-    echo "Installing MySQL ... SUCCESS"
-fi
-
-dnf install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "Installing Git... FAILURE"
-    exit 1
-else
-    echo "Installing Git ... SUCCESS"
-fi
-
+echo "Git is already ... installed"
 
