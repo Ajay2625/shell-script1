a456a4ba8a647493f56be8924a06388eb07c6cb6
Ajay2625 <ajayreddykondeti@gmail.com>
2025-03-15 22:53:46 -0700

    conversation added

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 FILE_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
@@ -15,10 +16,10 @@ LOG_FILE_NAME="$LOGS_FOLDER/$FILE_NAME-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo  -e "$2... $R FAILURE"
+        echo  -e "$2... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 ... $G SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
@@ -35,7 +36,7 @@ then
     VALIDATE $? "Installing MySQL"
     
 else
-echo -e "MySQL is already ... $Y installed"
+echo -e "MySQL is already ... $Y installed $N"
 
 fi
 
@@ -46,7 +47,7 @@ then
     dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing Git"
 else
-echo -e "Git is already ... $Y installed"
+echo -e "Git is already ... $Y installed $N"
 
 fi
 
